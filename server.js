49328761266ae7bf37799e33da35fe5ddc1e4040
DesmondDeon <youngdesmondd7@gmail.com>
2021-11-28 22:51:14 -0500

    Finished ver--0.1

---
+++ server.js
@@ -0,0 +1,58 @@
+// Dependencies
+// ==========================================================
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers')
+const sequelize = require('./config/connection')
+
+// Helpers
+const helpers = require('./utils/helpers');
+const sessions = require('express-session')
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+// APP and PORT
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Session Stores
+const SequelizeStore = require('connect-session-sequelize')(sessions.Store);
+
+const sess = {
+    secret: 'nagato',
+    cookie: {
+        expires: 10 * 60 * 1000
+    },
+    resave: true,
+    rollings: true,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    }),
+};
+
+app.use(session(sess));
+
+// Sets up the Express App
+// ==========================================================
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Sets Handlebars as the default template engine 
+// ==========================================================
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Controllers
+app.use(routes);
+
+// Starts the server to begin listening 
+// ==========================================================
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
